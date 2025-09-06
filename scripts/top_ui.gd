e63d420c4227af88a3369110513f2ca382e435e1
mazenAHL <mabuhamdan@ucsd.edu>
2025-09-06 09:10:52 -0400

    metodos base, movimientos y timer en top_ui

--- scripts/top_ui.gd
+++ scripts/top_ui.gd
@@ -3,5 +3,29 @@ extends TextureRect
 @onready var score_label = $MarginContainer/HBoxContainer/score_label
 @onready var counter_label = $MarginContainer/HBoxContainer/counter_label
 
-var current_score = 0
-var current_count = 0
+# top_ui.gd
+
+func set_score(n: int) -> void:
+	if score_label:
+		score_label.text = str(n)
+
+func set_moves(n: int) -> void:
+	if counter_label:
+		counter_label.text = str(n)
+
+func set_time(seconds: int) -> void:
+	if counter_label:
+		counter_label.text = str(seconds)
+
+func set_mode_moves() -> void:
+	if counter_label:
+		counter_label.text = "0"  
+		
+func set_mode_timed() -> void:
+	if counter_label:
+		counter_label.text = "0" 
+		
+func _ready() -> void:
+	set_score(0)       
+	set_mode_moves()    
+	set_moves(15)       
